51f2100d5cd7bf6b0c0f05fa27cfedbd97a6814f
Frisian-cmd <hsvdhoek@gmail.com>
2021-09-23 13:09:59 +0200

    init

--- capitolmcbot/main.js
+++ capitolmcbot/main.js
@@ -16,7 +16,7 @@ for(const file of commandFiles){
 
 client.on('ready', () => {
 	console.log('Ohayo senpai');
-	client.user.setActivity('LegendKIngdoms Minecraft server!', { type: "PLAYING" }).catch(console.error)
+	client.user.setActivity('LegendKingdoms Minecraft server!', { type: "PLAYING" }).catch(console.error)
 	memberCounter(client);
 });
 
