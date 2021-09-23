c63445dbbaee66a959656693d3672bac1a2df2bc
Frisian-cmd <hsvdhoek@gmail.com>
2021-09-23 10:59:59 +0200

    init

--- capitolmcbot/commands/map.js
+++ capitolmcbot/commands/map.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute(message, args, Discord){
 		const map = new Discord.MessageEmbed()
 		.setColor('#e67e22')
-		.setTitle('Hier is de Dynmap')
-		.setURL('http://legendkingdoms.serv.nu:65535/')
+		.setTitle('Hier is de map')
+		.setImage('https://cdn.discordapp.com/attachments/815958421693136946/890522221263601744/LegendKingdoms.png')
 		.setDescription('Alsjeblieft')
 		.setFooter('Bot development by Frisian™#7989')
 
